31055ab1211e39c6f8948c63ce61707fc1f37721
Bob Chen <main.bobchen@gmail.com>
2024-03-21 22:22:26 +1100

    updated home page progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Headshot from "../../public/IMG_3344.jpeg";
-import Headshot2 from "../../public/IMG_3344 copy.jpeg"
 import ABA from "../../public/IMG_5859.jpeg";
 import Accenture from "../../public/IMG_6939.jpg";
 import Fintech from '../../public/1708689451154.jpg'
 import DevSoc from "../../public/1708676099763.jpg"
 import Fintech2 from '../../public/1708689483040.jpg'
-import {Suspense} from "react";
+import React, {Suspense} from "react";
 
 interface ChannelLinkProps {
   img: string;
@@ -24,23 +23,50 @@ function ChannelLink(props: ChannelLinkProps) {
         target="_blank"
         className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
       >
-        <div className="flex items-center space-x-3">
-          <div className="relative h-16">
-            <Image
-              alt={name}
-              src={img}
-              height={64}
-              width={64}
-              sizes="33vw"
-              className="h-16 w-16"
-              priority
-            />
-          </div>
+        <div className="flex items-center space-x-5 flex-row align-middle">
+          {
+            img == "li" ? <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                               className="bi bi-linkedin" viewBox="0 0 16 16">
+              <path
+                d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+            </svg> : null
+          }
+          {
+            img == "we" ? <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                               className="bi bi-briefcase-fill" viewBox="0 0 16 16">
+              <path
+                d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5"/>
+              <path
+                d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85z"/>
+            </svg> : null
+          }
+          {
+            img == "oe" ? <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                               className="bi bi-chat-left-quote-fill" viewBox="0 0 16 16">
+              <path
+                d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm7.194 2.766a1.7 1.7 0 0 0-.227-.272 1.5 1.5 0 0 0-.469-.324l-.008-.004A1.8 1.8 0 0 0 5.734 4C4.776 4 4 4.746 4 5.667c0 .92.776 1.666 1.734 1.666.343 0 .662-.095.931-.26-.137.389-.39.804-.81 1.22a.405.405 0 0 0 .011.59c.173.16.447.155.614-.01 1.334-1.329 1.37-2.758.941-3.706a2.5 2.5 0 0 0-.227-.4zM11 7.073c-.136.389-.39.804-.81 1.22a.405.405 0 0 0 .012.59c.172.16.446.155.613-.01 1.334-1.329 1.37-2.758.942-3.706a2.5 2.5 0 0 0-.228-.4 1.7 1.7 0 0 0-.227-.273 1.5 1.5 0 0 0-.469-.324l-.008-.004A1.8 1.8 0 0 0 10.07 4c-.957 0-1.734.746-1.734 1.667 0 .92.777 1.666 1.734 1.666.343 0 .662-.095.931-.26z"/>
+            </svg> : null
+          }
+          {
+            img == "e" ? <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                              className="bi bi-file-earmark-word-fill" viewBox="0 0 16 16">
+              <path
+                d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M5.485 6.879l1.036 4.144.997-3.655a.5.5 0 0 1 .964 0l.997 3.655 1.036-4.144a.5.5 0 0 1 .97.242l-1.5 6a.5.5 0 0 1-.967.01L8 9.402l-1.018 3.73a.5.5 0 0 1-.967-.01l-1.5-6a.5.5 0 1 1 .97-.242z"/>
+            </svg> : null
+          }
+          {
+            img == "a" ?
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-stars"
+                   viewBox="0 0 16 16">
+                <path
+                  d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
+              </svg> : null
+          }
           <div className="flex flex-col">
             <p className="font-medium text-neutral-900 dark:text-neutral-100">
               {name}
             </p>
-            <Suspense fallback={<p className="h-6" />}>
+            <Suspense fallback={<p className="h-6"/>}>
               {site}
             </Suspense>
           </div>
@@ -65,12 +91,13 @@ export default function Home() {
         <a>work</a>
         <a>volunteering</a>
         <a>essays</a>
+        <a>achievements</a>
       </div>
-      <h1 className="px-6 py-8 text-2xl">Hello, I'm Bob 👋</h1>
+      <h1 className="px-6 py-8 text-3xl font-medium">Bob Chen</h1>
       <div className="columns-2 sm:columns-3 gap-4 my-8 px-6">
         <div className="relative h-40 mb-4">
           <Image
-            alt="Me speaking on stage at React Summit about the future of Next.js"
+            alt="My Software Development Society Team"
             src={DevSoc}
             fill
             priority
@@ -79,7 +106,7 @@ export default function Home() {
         </div>
         <div className="relative h-80 mb-4 sm:mb-0">
           <Image
-            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
+            alt="My Fintech Society Team"
             src={Fintech}
             fill
             priority
@@ -88,7 +115,7 @@ export default function Home() {
         </div>
         <div className="relative h-40 sm:h-80 sm:mb-4">
           <Image
-            alt="Me standing on stage at Reactathon delivering the keynote"
+            alt="Being awarded 2nd place at Accenture's Tech Bootcamp"
             src={Accenture}
             fill
             priority
@@ -97,7 +124,7 @@ export default function Home() {
         </div>
         <div className="relative h-40 mb-4 sm:mb-0">
           <Image
-            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
+            alt="Representing JADE at the 2023 Australian Bar Association's Annual Conference"
             src={ABA}
             fill
             priority
@@ -106,7 +133,7 @@ export default function Home() {
         </div>
         <div className="relative h-40 mb-4">
           <Image
-            alt="Me and Guillermo Rauch on stage for Vercel Ship, answering questions from the Next.js community"
+            alt="Achieving affiliation with the UNSW Business School"
             src={Fintech2}
             fill
             priority
@@ -115,7 +142,7 @@ export default function Home() {
         </div>
         <div className="relative h-80">
           <Image
-            alt="My badge on top of a pile of badges from a Vercel meetup we held"
+            alt="A photo of me."
             src={Headshot}
             fill
             priority
@@ -149,13 +176,13 @@ export default function Home() {
       </p>
       <div className="w-full px-6 py-8 columns-2">
         <ChannelLink
-          img="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+          img="li"
           name="LinkedIn"
           link="https://www.linkedin.com/in/3bobchen/"
           site="@3bobchen"
         />
         <ChannelLink
-          img="https://www.iconpacks.net/icons/2/free-briefcase-icon-1965-thumb.png"
+          img="we"
           name="Work Experiences"
           link="/"
           site=""
@@ -163,13 +190,13 @@ export default function Home() {
       </div>
       <div className="w-full px-6 pb-8 columns-2">
         <ChannelLink
-          img="https://cdn.icon-icons.com/icons2/1520/PNG/512/cogflat_106041.png"
+          img="oe"
           name="Other Experiences"
           link="https://www.linkedin.com/in/3bobchen/"
           site=""
         />
         <ChannelLink
-          img="https://cdn-icons-png.flaticon.com/512/1047/1047295.png"
+          img="e"
           name="Essays"
           link="https://www.linkedin.com/in/3bobchen/"
           site=""
@@ -177,7 +204,7 @@ export default function Home() {
       </div>
       <div className="w-full px-6 pb-8 columns-2">
         <ChannelLink
-          img="https://cdn-icons-png.freepik.com/512/8576/8576843.png"
+          img="a"
           name="Key Achievements"
           link="https://www.linkedin.com/in/3bobchen/"
           site=""
