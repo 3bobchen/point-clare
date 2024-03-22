215861929ff3b9679714182ec15b9b56d69f1a60
Bob Chen <main.bobchen@gmail.com>
2024-03-22 20:01:06 +1100

    adjust firebase setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'upload.wikimedia.org',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.iconpacks.net',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.icon-icons.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn-icons-png.flaticon.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn-icons-png.freepik.com',
-                port: '',
-                pathname: '/**',
-            }
-        ]
-    }
+        remotePatterns: []
+    },
+    output: "export"
 };
 
 export default nextConfig;
