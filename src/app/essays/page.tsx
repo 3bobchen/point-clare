44e33706929c422abf5baf8f5d8b7f5b412688a3
Bob Chen <main.bobchen@gmail.com>
2024-03-23 22:58:05 +1100

    essay landing

--- src/app/essays/page.tsx
+++ src/app/essays/page.tsx
@@ -1,4 +1,67 @@
-import React from "react";
+import React, {Suspense} from "react";
+import Image from "next/image";
+
+interface EssayLinkProps {
+  img: number;
+  link: string;
+  name: string;
+  subtitle: string;
+}
+
+function ChannelLink(props: EssayLinkProps) {
+  const {img, link, name, subtitle
+  } = props;
+  return (
+    <div className="group flex w-full py-3">
+      <a
+        href={link}
+        className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between w-full"
+      >
+        {
+          img == 1 ?
+            <Image src="https://agsci.oregonstate.edu/sites/agscid7/files/files/artwork/2002-06sandgren.jpg" alt=""
+                   width={300} height={100}/> : null
+        }
+        {
+          img == 2 ?
+            <Image src="https://i.pinimg.com/736x/5a/14/8b/5a148b6847922461b5bb93abdb244185.jpg" alt="" width={300} height={10} style={{objectFit: 'cover'}}/> : null
+        }
+        {
+          img == 3 ?
+            <Image src="https://media.thepeakmagazine.com.sg/public/2019/02/AI-creativity-paintings.jpg?compress=true&quality=80&w=1080&dpr=1" alt="" width={300} height={10} style={{objectFit: 'cover'}}/> : null
+        }
+        {
+          img == 4 ?
+            <Image src="https://www.kabgallery.com/system/wp-content/uploads/2016/01/%E2%80%9CBeach-Study-1-4.9.15-Plein-Air%E2%80%9D-45x45cm-oil-on-canvas-1500x1200.jpeg" alt="" width={300} height={10} style={{objectFit: 'cover'}}/> : null
+        }
+        <div className="flex flex-col self-start w-full p-5 justify-between h-full">
+          <div>
+            <p className="font-medium text-neutral-900 dark:text-neutral-100 text-xl py-2">
+              {name}
+            </p>
+            <Suspense fallback={<p className="h-6"/>}>
+              {subtitle}
+            </Suspense>
+          </div>
+          <p className="text-xs">
+            {
+              img == 1 ? "Imagery: A PRAGMATIC POETRY OF MIST, Erik Sandgren" : null
+            }
+            {
+              img == 2 ? "Imagery: Cubist 7p, Thomas C. Fedro" : null
+            }
+            {
+              img == 3 ? "Imagery: Righteous Anger, Ellen Downing" : null
+            }
+            {
+              img == 4 ? "Imagery: Beach Study, Sally West" : null
+            }
+          </p>
+        </div>
+      </a>
+    </div>
+  );
+}
 
 export default function Page() {
   return (
@@ -9,6 +72,32 @@ export default function Page() {
         <a href="/volunteering">volunteering</a>
         <a><b>essays</b></a>
         <a href="/achievements">achievements</a>
+      </div>
+      <div className="w-full px-6 pb-8">
+        <ChannelLink
+          img={1}
+          name="Packed, Stacked and Attacked"
+          link="https://www.linkedin.com/in/3bobchen/"
+          subtitle="The importance of administrative tribunal reform that learns from the past and sufficiently protects member independence"
+        />
+        <ChannelLink
+          img={2}
+          name="Central Bank Digital Currencies"
+          link="https://www.linkedin.com/in/3bobchen/"
+          subtitle="The new balance between privacy and access in the digital economy"
+        />
+        <ChannelLink
+          img={3}
+          name="Could an artificially intelligent device practise as a barrister in NSW?"
+          link="https://www.linkedin.com/in/3bobchen/"
+          subtitle=""
+        />
+        <ChannelLink
+          img={4}
+          name="I am, You are, We are Australian"
+          link="https://www.linkedin.com/in/3bobchen/"
+          subtitle="Indigenous settlement in a modern settler state"
+        />
       </div>
     </main>
   )
