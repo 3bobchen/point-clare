e8926fc42838ce7fbead35d6af47024911a05908
Bob Chen <main.bobchen@gmail.com>
2024-04-10 16:21:29 +1000

    volunteering info

--- src/app/volunteering/page.tsx
+++ src/app/volunteering/page.tsx
@@ -1,35 +1,71 @@
-import React, {Suspense} from "react";
+import React from "react";
 import Image from "next/image";
 import {work_data, WorkDataObject} from "@/app/volunteering/work_data";
 
-function WorkHistoryObject(props: {work: WorkDataObject}) {
+function WorkHistoryObject(props: { work: WorkDataObject }) {
   const {work} = props;
 
+  const months: string[] = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ]
+
+  let month_difference = 0
+  let year_difference = 0
+
+  const total_months1 = work.startYear * 12 + work.startMonth
+  const total_months2 = (work.endYear ?? new Date().getFullYear()) * 12 + (work.endMonth ?? new Date().getMonth())
+
+  month_difference = total_months2 - total_months1
+
+  while (month_difference >= 12) {
+    month_difference -= 12
+    year_difference += 1
+  }
+
   return (
-    <div className="group flex w-full py-3">
+    <div className="group flex-col w-full py-3">
       <div
-        // href=""
-        // target="_blank"
-        className="relative border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between w-full"
+        className="relative border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex-col w-full"
       >
-        <div className="absolute bg-white text-gray-600 px-2 rounded-lg right-3 top-3">{work.tag}</div>
-        <div className="px-6">
-          <Image src={work.companyLogo} alt=""
-                 width={300} height={100}/>
-        </div>
-        <div className="flex flex-col self-start w-full p-5 justify-between h-full">
-          <div>
-            <p className="font-medium text-neutral-900 dark:text-neutral-100 text-xl py-2">
-              {work.companyName}
-            </p>
-            <p className="text-neutral-900">
-              {work.location}
-            </p>
-            <p className="text-sm pt-2 text-neutral-600">
-              {work.companyBlurb}
-            </p>
+        <div className="flex items-center justify-between">
+          <div
+            className="absolute bg-white text-gray-600 px-2 rounded-lg right-3 top-3">{`${year_difference != 0 ? year_difference : ""} ${year_difference != 0 ? (year_difference > 1 ? "years" : "year") : ""} ${month_difference} ${month_difference > 1 ? "months" : "month"}`}</div>
+          <div className="px-6">
+            <Image src={work.companyLogo} alt=""
+                   width={300} height={100}/>
+          </div>
+          <div className="flex flex-col self-start w-full p-5 justify-between h-full">
+            <div>
+              <p className="font-medium text-neutral-900 dark:text-neutral-100 text-xl py-2">
+                {work.companyName}
+              </p>
+              <p className="text-neutral-900">
+                {work.location}
+              </p>
+              <p className="text-sm pt-2 text-neutral-600">
+                {work.companyBlurb}
+              </p>
+            </div>
           </div>
         </div>
+        {
+          work.roles.map((role, idx) => {
+            return (
+              <div>
+                <hr/>
+                <div className="my-5 mx-8">
+                  <div className="text-lg font-black">{role.roleTitle}</div>
+                  <p
+                    className="font-mono">{months[role.roleStartMonth - 1]} {role.roleStartYear} - {role.roleEndMonth ? months[role.roleEndMonth - 1] : null} {role.roleEndYear ?? "Present"}</p>
+                  <p>
+                    {role.roleDescription}
+                  </p>
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
--- src/app/volunteering/work_data.ts
+++ src/app/volunteering/work_data.ts
@@ -9,8 +9,6 @@ export interface WorkDataObject {
   endYear: number | null;
   location: string;
   roles: RoleObject[];
-  tag: string;
-  tagLink: string;
 }
 
 interface RoleObject {
@@ -34,8 +32,6 @@ export const work_data: WorkDataObject[] = [
     endMonth: null,
     endYear: null,
     location: "Kensington, NSW, Australia",
-    tag: "Not-for-Profit",
-    tagLink: "",
     roles: [
       {
         roleTitle: "Co-President",
@@ -44,6 +40,68 @@ export const work_data: WorkDataObject[] = [
         roleStartYear: 2023,
         roleEndMonth: null,
         roleEndYear: null,
+        roleDescription: ""
+      }
+    ]
+  },
+  {
+    companyName: "UNSW Fintech Society",
+    companyLogo: "https://framerusercontent.com/images/3jO1MjVcyxB6v1n5bd8IPcRPLI.png",
+    companyBlurb: "FinTech Soc is the pillar for representing the future of financial technologies and seasons of change within the university environment.",
+    companyWebsite: "https://devsoc.app/",
+    startMonth: 2,
+    startYear: 2022,
+    endMonth: 11,
+    endYear: 2023,
+    location: "Kensington, NSW, Australia",
+    roles: [
+      {
+        roleTitle: "Co-President",
+        roleSubtitle: "",
+        roleStartMonth: 11,
+        roleStartYear: 2022,
+        roleEndMonth: 11,
+        roleEndYear: 2023,
+        roleDescription: ""
+      },
+      {
+        roleTitle: "Events Director",
+        roleSubtitle: "",
+        roleStartMonth: 2,
+        roleStartYear: 2022,
+        roleEndMonth: 11,
+        roleEndYear: 2022,
+        roleDescription: ""
+      }
+    ]
+  },
+  {
+    companyName: "Computer Science Engineering Society",
+    companyLogo: "https://cdn-static.revolutionise.com.au/assets/images/unsw-csesoc.png",
+    companyBlurb: "CSESoc is one of the biggest and most active societies at UNSW, catering to over 3500 CSE students spanning across degrees in Computer Science.",
+    companyWebsite: "https://devsoc.app/",
+    startMonth: 2,
+    startYear: 2021,
+    endMonth: 11,
+    endYear: 2023,
+    location: "Kensington, NSW, Australia",
+    roles: [
+      {
+        roleTitle: "Project Lead - Freerooms",
+        roleSubtitle: "",
+        roleStartMonth: 11,
+        roleStartYear: 2022,
+        roleEndMonth: 11,
+        roleEndYear: 2023,
+        roleDescription: ""
+      },
+      {
+        roleTitle: "Creative Subcommittee",
+        roleSubtitle: "",
+        roleStartMonth: 2,
+        roleStartYear: 2021,
+        roleEndMonth: 11,
+        roleEndYear: 2021,
         roleDescription: ""
       }
     ]
