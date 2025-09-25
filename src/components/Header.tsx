aa59d1cf092df4c6d9863e6099a43d86e07ea729
Bob Chen <main.bobchen@gmail.com>
2025-09-25 15:22:48 +1000

    Header glassui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,41 +4,66 @@ export function Header(props: { current: string }) {
   const { current } = props;
 
   return (
-    <div className="fixed top-5 right-5 h-14 text-gray-50 rounded-3xl bg-black/30 backdrop-blur-md border border-white/10 flex items-center space-x-4 px-6 shadow-sm z-50">
+    <div className="fixed top-5 right-2 sm:right-5 h-14 text-gray-50 rounded-3xl bg-black/30 backdrop-blur-md border border-white/10 flex items-center px-6 shadow-sm z-50">
       {current === "home" ? (
-        <a>
+        <a className="relative px-1 sm:px-3 py-1 rounded-full text-sm text-white hover:text-gray-200 before:absolute before:inset-0 before:rounded-full before:bg-white/30  before:z-0 z-10">
           <b>home</b>
         </a>
       ) : (
-        <a href="/">home</a>
+        <a
+          href="/"
+          className="relative px-1 sm:px-3 hover:rounded-full text-sm hover:text-gray-200 hover:before:absolute hover:before:inset-0 hover:before:rounded-full hover:before:bg-white/30  hover:before:z-0 hover:z-10"
+        >
+          home
+        </a>
       )}
       {current === "highlights" ? (
-        <a>
+        <a className="relative px-1 sm:px-3 rounded-full text-sm text-white hover:text-gray-200 before:absolute before:inset-0 before:rounded-full before:bg-white/30  before:z-0 z-10">
           <b>highlights</b>
         </a>
       ) : (
-        <a href="/achievements">highlights</a>
+        <a
+          href="/achievements"
+          className="relative px-1 sm:px-3 hover:rounded-full text-sm hover:text-gray-200 hover:before:absolute hover:before:inset-0 hover:before:rounded-full hover:before:bg-white/30  hover:before:z-0 hover:z-10"
+        >
+          highlights
+        </a>
       )}
       {current === "travel" ? (
-        <a>
+        <a className="relative px-1 sm:px-3 rounded-full text-sm text-white hover:text-gray-200 before:absolute before:inset-0 before:rounded-full before:bg-white/30  before:z-0 z-10">
           <b>travel</b>
         </a>
       ) : (
-        <a href="/travel">travel</a>
+        <a
+          href="/travel"
+          className="relative px-1 sm:px-3 hover:rounded-full text-sm hover:text-gray-200 hover:before:absolute hover:before:inset-0 hover:before:rounded-full hover:before:bg-white/30  hover:before:z-0 hover:z-10"
+        >
+          travel
+        </a>
       )}
       {current === "work" ? (
-        <a>
+        <a className="relative px-1 sm:px-3 rounded-full text-sm text-white hover:text-gray-200 before:absolute before:inset-0 before:rounded-full before:bg-white/30  before:z-0 z-10">
           <b>work</b>
         </a>
       ) : (
-        <a href="/work">work</a>
+        <a
+          href="/work"
+          className="relative px-1 sm:px-3 hover:rounded-full text-sm hover:text-gray-200 hover:before:absolute hover:before:inset-0 hover:before:rounded-full hover:before:bg-white/30  hover:before:z-0 hover:z-10"
+        >
+          work
+        </a>
       )}
       {current === "volunteering" ? (
-        <a>
+        <a className="relative px-1 sm:px-3 rounded-full text-sm text-white hover:text-gray-200 before:absolute before:inset-0 before:rounded-full before:bg-white/30  before:z-0 z-10">
           <b>volunteering</b>
         </a>
       ) : (
-        <a href="/volunteering">volunteering</a>
+        <a
+          href="/volunteering"
+          className="relative px-1 sm:px-3 hover:rounded-full text-sm hover:text-gray-200 hover:before:absolute hover:before:inset-0 hover:before:rounded-full hover:before:bg-white/30  hover:before:z-0 hover:z-10"
+        >
+          volunteering
+        </a>
       )}
     </div>
   );
